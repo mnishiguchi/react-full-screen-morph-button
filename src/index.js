a6a49ca8e97fb2cb646a9a69053d140bdba9a982
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2016-12-30 08:34:01 -0500

    Extract morph button functionalities from codrops example code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
+// Global styles
+import 'bootstrap/dist/css/bootstrap.css'
 import './index.css';
 
 ReactDOM.render(
