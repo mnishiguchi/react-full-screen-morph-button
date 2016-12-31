e004dc5a8472e259b3157526d12f95363fa597fa
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2016-12-31 10:43:22 -0500

    Get FullPageMorphButton component to open

--- src/FullPageMorphButton/FullPageMorphButton.js
+++ src/FullPageMorphButton/FullPageMorphButton.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import classie from 'classie'
-import UIMorphingButton from './lib/ui-morphing-button'
+import classNames from 'classnames'
+import UIMorphingButton from '../lib/ui-morphing-button'
 
 import './FullPageMorphButton.css'
 
@@ -9,17 +10,27 @@ class FullPageMorphButton extends React.Component {
   constructor(props) {
     super(props)
 
-    this.state = {}
+    this.state = {
+      isExpanded : false,
+      isAnimating: false,
+    }
   }
 
   render() {
+    const FullPageMorphButtonClassName = classNames({
+      'FullPageMorphButton' : true,
+      'open'                : !this.state.isExpanded && this.state.isAnimating,
+      'open scroll'         : this.state.isExpanded,
+    })
+
     return (
-      <div className="FullPageMorphButton">
+      <div className={FullPageMorphButtonClassName}>
 
         <button
           type="button"
           className="openButton"
-          >
+          onClick={e => this._handleOpenButtonClick(e)}
+        >
           {this.props.buttonText}
         </button>
 
@@ -35,10 +46,16 @@ class FullPageMorphButton extends React.Component {
 
 
   componentDidMount() {
-    this._initUIMorphingButton()
+    // this._initUIMorphingButton()
+
+    this._buttonEl  = document.querySelector('.FullPageMorphButton .openButton')
+    this._contentEl = document.querySelector('.FullPageMorphButton .morph-content')
   }
 
-  componentWillUnmount() {
+
+  shouldComponentUpdate(nextProps, nextState) {
+    // No updating after animation
+    return nextState.isExpanded && !nextState.isAnimating
   }
 
 
@@ -51,10 +68,28 @@ class FullPageMorphButton extends React.Component {
     const wrapperEl = document.querySelector( '.FullPageMorphButton' )
     const config = {
       closeSelector: '#close-button',
-      onAfterOpen: () => { classie.addClass( wrapperEl, 'scroll' ) },
+      onAfterOpen  : () => { classie.addClass( wrapperEl, 'scroll' ) },
     }
 
     new UIMorphingButton( wrapperEl, config )
+  }
+
+  _handleOpenButtonClick(event) {
+    console.log(event.target)
+    this.setState({ isAnimating: true })
+
+    // Disable the transition initially.
+    classie.addClass(this._contentEl, 'no-transition')
+
+    // Align the content to the button.
+    const bottonPosition  = this._buttonEl.getBoundingClientRect()
+    this._contentEl.style.top  = `${bottonPosition.top}px`
+    this._contentEl.style.left = `${bottonPosition.left}px`
+
+    setTimeout(() => {
+        classie.removeClass(this._contentEl, 'no-transition')
+        this.setState({ isExpanded: true, isAnimating: false })
+    }, 25)
   }
 
 } // end class
